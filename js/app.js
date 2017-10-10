5ae47eb87e468fe7706b89e3fb00c054439c4b29
Saarim Zaman <saarim.z@gmail.com>
2017-10-10 18:21:55 -0400

    added a cross button for input field

--- js/app.js
+++ js/app.js
@@ -32,14 +32,11 @@ $(document).ready(function(){
         self.searchLimit = ko.observableArray([10,20,30,40,50,100]);
         self.limitValue = ko.observable();
 
-<<<<<<< HEAD
-        //to do: add a dropdown for category
-=======
         //api request
         self.requestHappening = ko.observable(false);
         self.requestFailed = ko.observable(false);
->>>>>>> ec9b63881befbf4c1c6626454225b80b5c5006e2
 
+        //search bar search
         self.search = () => {
             let search = self.itemToAdd();
             let limit = self.limitValue();
@@ -47,12 +44,17 @@ $(document).ready(function(){
             self.foursquareSearch(url);
         }
 
+        self.clearInputField = () => {
+            self.itemToAdd(null);
+        }
+
         self.clearList = () => {
             //remvoe markers
             self.listItems().forEach((item) =>{
                 item.marker.setMap(null);
             });
-
+            //clear input field 
+            self.clearInputField();
             //remove item from list
             self.listItems.removeAll();
         }
@@ -63,7 +65,7 @@ $(document).ready(function(){
 
         self.isNotEmpty = ko.computed(() => {
             return (self.listItems().length);
-        })
+        });
 
 
         self.removeItem = (item) => {
@@ -71,8 +73,7 @@ $(document).ready(function(){
             item.marker.setMap(null);
             //item
             self.listItems.remove(item);
-            
-        }
+        };
 
         self.currentLocationSearch = () => {
             navigator.geolocation.getCurrentPosition(function(position){
@@ -81,50 +82,39 @@ $(document).ready(function(){
                 let url = `https://api.foursquare.com/v2/venues/search?v=20171006&client_secret=BYTSHE2RSR3PRO0EQASUDEMRJMIWBKQHT40XR30O4KHUHH4P&client_id=KF23DOLA3ZF03UHQCP5SNZBXFHQVLMIK1RV5S5XEMOUGWBXS&limit=${limit}&ll=${ll}`;
                 self.foursquareSearch(url);
             });
-        }
+        };
 
         self.foursquareSearch = (url) => {
-<<<<<<< HEAD
-            //TO DO: convert this to Knockout
-            $(".fa-spinner").toggleClass("hidden");
-
-=======
+            //start spinner 
             self.requestHappening(true);
->>>>>>> ec9b63881befbf4c1c6626454225b80b5c5006e2
             fetch(url).then((response) => {
                 return response.json();
             }).then((data) => {
                 let bounds = new google.maps.LatLngBounds();
                 self.clearList();
                 data.response.venues.forEach((venue) => {
+                    //signal end of request for getting rid of spinener
                     self.requestHappening(false);
+                    //create venue obj
                     let venueObj = new ListData(venue)
                     self.listItems.push(venueObj);
                     bounds.extend(venueObj.marker.position);
-                    //clear item after search
-                    self.itemToAdd(null);
+                    //update requestfailed observable
                     self.requestFailed(false);
                 });
-<<<<<<< HEAD
-                //TO DO: convert this to Knockout
-                $(".fa-spinner").toggleClass("hidden");
-
-                map.fitBounds(bounds);
-            }).catch((e) =>{
-                //TO DO: convert this to Knockout
-                $(".fa-spinner").toggleClass("hidden");
-                $(".list-view").html("<li>Not Found</li>");
-
-=======
                 map.fitBounds(bounds);
             }).catch((e) => {
+                //end loading spinner
                 self.requestHappening(false);
+                //clear input field after search
+                self.clearInputField();
+                //update requestfailed observable
                 self.requestFailed(true);
->>>>>>> ec9b63881befbf4c1c6626454225b80b5c5006e2
+                //log in console
                 console.log(e);
             });
             
-        }
+        };
     } 
 
     //ko apply bindings
